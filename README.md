52daba1b5ae511b848fb514ca3c7a9b162a42ddb
djdeo <deoseoer@gmail.com>
2018-01-12 13:56:31 +0800

    add app.vue

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # vue-cli-fetch-json
+
 Fetch json data with vue-cli webpack
+
+```
+# init vue
+$ vue init webpack-simple my-project
+$ cd my-project
+$ npm install
+$ npm run dev
+```
+
+
+replace `index.html` and `App.vue` file with the same file in this repo, then 
+
+```
+npm run build
+```
+
+then add data into `posts.json` directly in future work
---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <h1>All the posts</h1>
+    <div v-for="post in posts">
+      <h2> {{post.title}} </h2>
+      <small> <em>{{post.date}}</em> </small>
+      <p> {{post.body}} </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      posts: []
+    }
+  },
+  created () {
+    fetch('/posts.json')
+    .then(res => res.json())
+    .then(data => {
+      this.posts = data
+      console.log(data)
+    })
+  }
+}
+</script>
+
+<style>
+
+body{
+  max-width: 680px;
+  margin: 0 auto;
+}
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  border:1px solid gray;
+  padding: 10px;
+}
+
+h1{
+  border-bottom: 2px black solid;
+  padding-bottom: 10px;
+}
+
+</style>
